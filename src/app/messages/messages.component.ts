0d86a5c12c7f97ce44eaeb1b39a76f0c4cc8a016
GiorgioM98 <marian-gio@libero.it>
2024-04-30 13:32:47 +0200

    applicazione angular tour of heroes

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css'
+})
+export class MessagesComponent {
+  constructor(public messageService: MessageService) { }
+
+}
